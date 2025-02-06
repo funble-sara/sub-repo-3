ee291f24e84051d9e23ead44954feb860da0900a
seoha <sara@funble.kr>
2025-02-06 19:16:43 +0900

    change App.js

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           THIS IS A MAIN REPOSITORY
+          This is a sub repository
         </p>
         <a
           className="App-link"
